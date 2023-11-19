cd2ebeb60500c7990cd72a25671a305128937f99
Moh1tsingh <thakurmohitsingh2003@gmail.com>
2023-11-20 00:23:21 +0530

    Added login,About page and crated frontend for whole site

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,17 @@
-import React from 'react'
-import menu from "../assets/menu.png"
+import React from "react";
+import menu from "../assets/menu.png";
+import { Link, NavLink } from "react-router-dom";
+import back from "../assets/Arrow.png";
+
+const toggleMenu = () => {
+  const navItems = document.querySelector(".hamburger-container");
+  navItems.style.display = "flex";
+};
+
+const toggleMenuClose = () => {
+  const navItems = document.querySelector(".hamburger-container");
+  navItems.style.display = "none";
+};
 
 const Nav = () => {
   return (
@@ -7,16 +19,45 @@ const Nav = () => {
       <div className="navbar">
         <a className="icon">Icon</a>
         <div className="nav-items">
-          <a href="/">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
+          <NavLink className="Navlink" to="/">
+            Home
+          </NavLink>
+          <NavLink className="Navlink" to="/about">
+            About
+          </NavLink>
+          <NavLink className="Navlink" to="/login">
+            Login
+          </NavLink>
         </div>
         <div className="nav-items-2">
-          <img src={menu} alt="" />
+          <img
+            className="hamburger-icon"
+            src={menu}
+            alt=""
+            onClick={toggleMenu}
+          />
+        </div>
+      </div>
+      <div className="hamburger-container">
+        <div className="nav-items-mobile">
+          <NavLink className="Navlink" to="/" onClick={toggleMenuClose}>
+            Home
+          </NavLink>
+          <NavLink className="Navlink" to="/about" onClick={toggleMenuClose}>
+            About
+          </NavLink>
+          <NavLink className="Navlink" to="/login" onClick={toggleMenuClose}>
+            Login
+          </NavLink>
+          <img
+            className="GoBackArrow"
+            src={back}
+            alt=""
+            onClick={toggleMenuClose}
+          />
         </div>
       </div>
     </div>
   );
-}
-
-export default Nav
+};
+export default Nav;
